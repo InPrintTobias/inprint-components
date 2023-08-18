ad925701b9051f9579789897a97da365deb29307
InPrintTobias <to@inprint.dk>
2023-08-18 11:18:17 +0000

    Aded

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+    <button :type="type" class="transition-color duration-100 inline-flex items-center w-full justify-center gap-x-1.5 rounded-md bg-gray-100 px-3 py-1.5 text-sm font-semibold text-gray-700 hover:bg-gray-400 hover:text-white">
+        <slot />
+        <component :is="icon" v-if="icon" class="w-4 h-4 stroke-2"></component>
+    </button>
+</template>
+<script>
+export default {
+    props: {
+        icon: {
+            required: false,
+            type: Object,
+        },
+        type: {
+            required: false,
+            type: String,
+            default: 'submit',
+        },
+    }
+}
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), dts()],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "TestComponent",
+      fileName: "test-component",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
